b6827daf7830326e28d3bbe28dcc7110a3eacdd9
Orçun KILIÇASLAN <orcunkilicaslan@icloud.com>
2021-01-09 07:40:57 +0300

    Nav Menu Smooth Scroll Added

--- assets/src/script/custom/app.js
+++ assets/src/script/custom/app.js
@@ -1,4 +1,24 @@
 
-/* Wizard */
+/* Smooth Scroll to Div */
+$(document).on('click', '.header a[data-target]', function(e){
+    e.preventDefault();
 
+    var target  = $(this).attr('data-target');
+    var href    = $(this).attr('href');
+    var $target = $(target);
+
+    if ($target.length === 0) {
+        //console.log('sayfayok');
+        window.location.href = href;
+        //return;
+    }else{
+        //console.log('sayfavar');
+        var pos = $target.offset().top;
+        $('body, html').animate({scrollTop: pos});
+    }
+
+});
+/* Smooth Scroll to Div */
+
+/* Wizard */
 /* Wizard */
